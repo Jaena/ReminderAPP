3a479605931f1f49a60a5f066463b8a5061db8dc
yohan5050 <yohan5050@naver.com>
2017-07-20 20:28:40 +0900

    VoicePlayer class comment English->Korean

--- app/src/main/java/com/google/cloud/android/reminderapp/VoicePlayer.java
+++ app/src/main/java/com/google/cloud/android/reminderapp/VoicePlayer.java
@@ -11,7 +11,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
- * This class receives the recorded file name from DB and plays the file.
+ * 이 클래스는 DB로부터 녹음된 파일명을 받아와 해당 파일을 재생하는 역할을 수행한다.
  */
 
 public class VoicePlayer {
@@ -27,24 +27,19 @@ public class VoicePlayer {
     private Thread mPlayingThread = null;
     int playCount;
 
-    /**
-     * Constructor of the VoicePlayer class.
-     * Get context and db instance from MainActivity class.
-     *
-     * @param c    context(the information of this program)
-     */
     VoicePlayer(Context c)
     {
         context = c;
         db = MainActivity.getDBInstance();
     }
 
+    //TODO : 피어리뷰에 적어야 할 것들 minBufferSize - 사용되지 않는 변수 제거, 불필요한 AudioTrack인스턴스 생성
     /**
-     * Called to call the playWaveFile method on a thread for playing audio files.
-     * This method creates a new thread and plays the playWaveFile method on the thread.
+     * 이 메소드는 새로운 thread를 생성하여 playWaveFile 메소드를 실행한다.
+     * 음성 파일을 재생하기 위해 호출된다.
      *
-     * @param SampleRate    sample rate expressed in Hertz.
-     * @param mBufferSize    total size of the buffer where audio data is written to during the recording.
+     * @param SampleRate     녹음 시 사용된 sample rate(Hertz)
+     * @param mBufferSize    재생 시 음성 파일에서 한 번에 읽어오는 음성 데이터의 최대 크기
      */
     public void startPlaying(final int SampleRate, final int mBufferSize) {
         int minBufferSize = AudioTrack.getMinBufferSize(SampleRate, CHANNEL, ENCODING);
@@ -60,9 +55,9 @@ public class VoicePlayer {
         mPlayingThread.start();
     }
 
+    //TODO 변수 playing을 isplaying으로 바꾸기 - 피어리뷰에도 적기
     /**
-     * Called to stop playing audio files.
-     * This method stops playing audio by initializing variable playing to false.
+     * 이 메소드는 변수 playing을 false로 설정하여 재생을 중지한다.
      */
     public void stopPlaying()
     {
@@ -70,11 +65,11 @@ public class VoicePlayer {
     }
 
     /**
-     * Called to playback audio files.
-     * This method creates an AudioTrack instance using the sample rate used when recording and playbacks the data from the audio file by the buffer size.
+     * 이 메소드는 녹음 시 사용된 sample rate에 따라 audioTrack instance를 생성하고 음성 파일로부터 buffer size만큼 정보를 읽어와 재생한다.
+     * 변수 playing의 값이 true일 때 재생한다.
      *
-     * @param SampleRate    sample rate expressed in Hertz.
-     * @param mBufferSize    total size of the buffer where audio data is written to during the recording
+     * @param SampleRate     녹음 시 사용된 sample rate(Hertz)
+     * @param mBufferSize    재생 시 음성 파일에서 한 번에 읽어오는 음성 데이터의 최대 크기
      *
      * @exception FileNotFoundException
      * @exeption IOException
@@ -109,10 +104,9 @@ public class VoicePlayer {
     }
 
     /**
-     * Called to check whether audio is being played now.
-     * This method returns whether audio is being played now.
+     * 이 메소드는 현재 재생 중인지 아닌지를 판단하기 위해 호출된다.
      *
-     * @return boolean    whether audio is being played now.
+     * @return boolean    현재 재생 중 여부 (True : 재생 중, False : 재생 중 아님)
      */
     public boolean isPlaying()
     {
